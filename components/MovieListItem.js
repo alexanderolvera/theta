eaae6537fec335f1cceece8bf2f006dc81cacd2a
alexanderolvera <alexander.olvera943@gmail.com>
2021-12-07 21:22:46 -0600

    added movielistitem component

---
+++ components/MovieListItem.js
@@ -0,0 +1,14 @@
+export default function MovieListItem({ movieData }) {
+  return (
+    <li className="list-group-item">
+      <div className="d-flex flex-row">
+        <img className="me-1 rounded" src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} height="200px"></img>
+        <div className="details d-flex flex-column me-2">
+          <h1 className="me-auto">{movieData.title}</h1>
+          <p>{movieData.overview}</p>
+          <button className="btn btn-primary btn-sm add-button">Add+</button>
+        </div>
+      </div>
+    </li>
+  )
+}

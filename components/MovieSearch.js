dff5a1ce4e6c2fa0d80e3488d60a7b4d7cbd198e
alexanderolvera <alexander.olvera943@gmail.com>
2021-12-07 21:23:24 -0600

    Updated to get movie search data and load a list

--- components/MovieSearch.js
+++ components/MovieSearch.js
@@ -1,14 +1,48 @@
+import { useState } from 'react'
+import axios from 'axios'
+import MovieList from "./MovieList"
+import toast from 'react-hot-toast'
+import Loader from './Loader'
+
 export default function MovieSearch() {
+  const [searchQuery, setSearchQuery] = useState(null)
+  const [movieList, setMovieList] = useState(null)
+  const [searching, setSearching] = useState(false)
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const handleSearchExecute = async (e) => {
+    try {
+      setSearching(true)
+      let movieData = await axios.get('/api/search', {
+        params: { searchQuery }
+      })
+      setMovieList(movieData.data)
+      setSearching(false)
+    } catch (error) {
+      setSearching(false)
+      console.log(error)
+      toast.error(`Issue searching for ${searchQuery}`)
+    }
+  }
+
   return (
     <>
       <h3>Movie Search Component</h3>
       <div className="row g-3 align-items-center">
           <div className="col-auto">
-            <label className="col-form-label">Search</label>
+            <button className="btn btn-info" onClick={handleSearchExecute}>Search</button>
           </div>
           <div className="col-auto">
-            <input type="search" id="search" className="form-control" />
+            <input type="search" id="search" className="form-control" onChange={handleSearchChange}/>
           </div>
+          <Loader show={searching}/>
+      </div>
+      <div className="search-data">
+        {/* pass movie list from api call here */}
+        <MovieList list={movieList}/>
       </div>
     </>
   )

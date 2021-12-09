f158f988067f1a0956381add5ba50df752a12e26
alexanderolvera <alexander.olvera943@gmail.com>
2021-12-09 17:52:59 -0600

    Updated styles for navbar. Removed unessesary options

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,34 +25,22 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link href={'/'} passHref>
+          <a className="navbar-brand mb-0 h1 border rounded text-white bg-dark">Marquee</a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          {user && (
+            <>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+              <Link href={`/${user.uid}/dashboard`}>
+                <a className={`nav-link ${router.pathname.includes('dashboard') && 'active'}`} href="#">Dashboard</a>
+              </Link>
             </li>
           </ul>
-          {user && (
             <ul className="navbar-nav ms-auto">
               <li className="my-auto me-2">
                 <button className="btn btn-danger" onClick={handleSignOut}>Log Out</button>
@@ -63,6 +51,7 @@ export default function Navbar() {
                 </Link>
               </li>
             </ul>
+            </>
           )}
           {/* user is not signed OR has not created username */}
           {
